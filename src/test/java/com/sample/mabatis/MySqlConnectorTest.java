5d2d23142c5a39c20b82c52b0844b3057e80c2af
jwKim <jw4005.kim@gmail.com>
2018-04-04 05:21:14 +0900

    환경설정

--- src/test/java/com/sample/mabatis/MySqlConnectorTest.java
+++ src/test/java/com/sample/mabatis/MySqlConnectorTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = { "file:/web/WEB-INF/spring/root-context.xml" })
 public class MySqlConnectorTest {
 
-    @Inject
+/*    @Inject
     private DataSource dataSource;
 
     @Test
@@ -25,6 +25,6 @@ public class MySqlConnectorTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
+    }*/
 
 }
---
+++ web/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jwKim
+  Date: 2018-04-04
+  Time: 오전 4:44
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    Hellow JwWorld
+</body>
+</html>
