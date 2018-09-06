c1ca37eefeafc4e25dab0170634deb4c120fe4b1
JW-Kim <jw4005.kim@gmail.com>
2018-09-06 19:36:32 +0900

    Create Diary.java

---
+++ src/main/java/com/product/luffy/po/Diary.java
@@ -0,0 +1,301 @@
+package com.product.luffy.po;
+
+import java.sql.Date;
+
+public class Diary {
+	private String diaryId;
+	private String title;
+	private String content;
+	private String regUserId;
+	private String fileId;
+	private String stateId;
+	private String feelingCd;
+	private String healthCd;
+	private String feverCd;
+	private String breakfastCd;
+	private String lunchCd;
+	private String dinnerCd;
+	private String shitCd;
+	private String shitCnt;
+	private String shitDesc;
+	private Date sleepStartDtm;
+	private Date sleepEndDtm;
+	private Date regDtm;
+	
+	
+	
+	
+	public String getDiaryId() {
+		return diaryId;
+	}
+
+
+
+
+	public void setDiaryId(String diaryId) {
+		this.diaryId = diaryId;
+	}
+
+
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+
+
+	public String getContent() {
+		return content;
+	}
+
+
+
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+
+
+
+	public String getRegUserId() {
+		return regUserId;
+	}
+
+
+
+
+	public void setRegUserId(String regUserId) {
+		this.regUserId = regUserId;
+	}
+
+
+
+
+	public String getFileId() {
+		return fileId;
+	}
+
+
+
+
+	public void setFileId(String fileId) {
+		this.fileId = fileId;
+	}
+
+
+
+
+	public String getStateId() {
+		return stateId;
+	}
+
+
+
+
+	public void setStateId(String stateId) {
+		this.stateId = stateId;
+	}
+
+
+
+
+	public String getFeelingCd() {
+		return feelingCd;
+	}
+
+
+
+
+	public void setFeelingCd(String feelingCd) {
+		this.feelingCd = feelingCd;
+	}
+
+
+
+
+	public String getHealthCd() {
+		return healthCd;
+	}
+
+
+
+
+	public void setHealthCd(String healthCd) {
+		this.healthCd = healthCd;
+	}
+
+
+
+
+	public String getFeverCd() {
+		return feverCd;
+	}
+
+
+
+
+	public void setFeverCd(String feverCd) {
+		this.feverCd = feverCd;
+	}
+
+
+
+
+	public String getBreakfastCd() {
+		return breakfastCd;
+	}
+
+
+
+
+	public void setBreakfastCd(String breakfastCd) {
+		this.breakfastCd = breakfastCd;
+	}
+
+
+
+
+	public String getLunchCd() {
+		return lunchCd;
+	}
+
+
+
+
+	public void setLunchCd(String lunchCd) {
+		this.lunchCd = lunchCd;
+	}
+
+
+
+
+	public String getDinnerCd() {
+		return dinnerCd;
+	}
+
+
+
+
+	public void setDinnerCd(String dinnerCd) {
+		this.dinnerCd = dinnerCd;
+	}
+
+
+
+
+	public String getShitCd() {
+		return shitCd;
+	}
+
+
+
+
+	public void setShitCd(String shitCd) {
+		this.shitCd = shitCd;
+	}
+
+
+
+
+
+	public String getShitCnt() {
+		return shitCnt;
+	}
+
+
+
+
+	public void setShitCnt(String shitCnt) {
+		this.shitCnt = shitCnt;
+	}
+
+
+
+
+	public String getShitDesc() {
+		return shitDesc;
+	}
+
+
+
+
+	public void setShitDesc(String shitDesc) {
+		this.shitDesc = shitDesc;
+	}
+
+
+
+
+	public Date getSleepStartDtm() {
+		return sleepStartDtm;
+	}
+
+
+
+
+	public void setSleepStartDtm(Date sleepStartDtm) {
+		this.sleepStartDtm = sleepStartDtm;
+	}
+
+
+
+
+	public Date getSleepEndDtm() {
+		return sleepEndDtm;
+	}
+
+
+
+
+	public void setSleepEndDtm(Date sleepEndDtm) {
+		this.sleepEndDtm = sleepEndDtm;
+	}
+
+
+
+
+	public Date getRegDtm() {
+		return regDtm;
+	}
+
+
+
+
+	public void setRegDtm(Date regDtm) {
+		this.regDtm = regDtm;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "Diary [diaryId=" + diaryId + ", title=" + title + ", content=" + content + ", regUserId=" + regUserId
+				+ ", fileId=" + fileId + ", stateId=" + stateId + ", feelingCd=" + feelingCd + ", healthCd=" + healthCd
+				+ ", feverCd=" + feverCd + ", breakfastCd=" + breakfastCd + ", lunchCd=" + lunchCd + ", dinnerCd="
+				+ dinnerCd + ", shitCd=" + shitCd + ", shitCnt=" + shitCnt + ", shitDesc=" + shitDesc
+				+ ", sleepStartDtm=" + sleepStartDtm + ", sleepEndDtm=" + sleepEndDtm + ", regDtm=" + regDtm
+				+ ", getDiaryId()=" + getDiaryId() + ", getTitle()=" + getTitle() + ", getContent()=" + getContent()
+				+ ", getRegUserId()=" + getRegUserId() + ", getFileId()=" + getFileId() + ", getStateId()="
+				+ getStateId() + ", getFeelingCd()=" + getFeelingCd() + ", getHealthCd()=" + getHealthCd()
+				+ ", getFeverCd()=" + getFeverCd() + ", getBreakfastCd()=" + getBreakfastCd() + ", getLunchCd()="
+				+ getLunchCd() + ", getDinnerCd()=" + getDinnerCd() + ", getShitCd()=" + getShitCd() + ", getShitCnt()="
+				+ getShitCnt() + ", getShitDesc()=" + getShitDesc() + ", getSleepStartDtm()=" + getSleepStartDtm()
+				+ ", getSleepEndDtm()=" + getSleepEndDtm() + ", getRegDtm()=" + getRegDtm() + ", getClass()="
+				+ getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	}
+
+	
+	
+	
+}
