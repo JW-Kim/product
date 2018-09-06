d473ec6d1b7847faf96524a081b560728d5a643d
JW-Kim <jw4005.kim@gmail.com>
2018-09-06 16:06:22 +0900

    Update UserController.java

--- src/main/java/com/product/luffy/controller/UserController.java
+++ src/main/java/com/product/luffy/controller/UserController.java
@@ -11,12 +11,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.product.luffy.po.User;
 import com.product.luffy.service.UserService;
+import com.product.luffy.utils.Exception.ProductRuntimeException;
 import com.product.luffy.utils.response.GridResponseObject;
 import com.product.luffy.utils.response.HttpResultCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 	
 	@Resource(name="com.product.luffy.service.impl.UserService")
 	UserService userService;
@@ -24,9 +29,13 @@ public class UserController {
 	@RequestMapping(method = RequestMethod.GET)
 	public @ResponseBody GridResponseObject<User> selectUserList() {
 		GridResponseObject<User> gridResponseObject = new GridResponseObject<User>();
-		System.out.println(">>>> selectUserList 시작");
+		LOGGER.debug(">>>> selectUserList 시작");
 		
 		List<User> userList = userService.selectUserList();
+		
+/*		if(userList.size() == 2) {
+			throw new ProductRuntimeException(HttpResultCode.PRODUCT_CONFLICT, "test");
+		}*/
 		gridResponseObject.setData(userList);
 		gridResponseObject.setResultCode(HttpResultCode.PRODUCT_SUCCESS);
 		System.out.println(">>>> selectUserList 끝");
