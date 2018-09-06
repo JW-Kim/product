28745a3916cc0c4ec65169331acf5af17b32eb1c
JW-Kim <jw4005.kim@gmail.com>
2018-09-06 16:04:21 +0900

    Update ResponseObject.java

--- src/main/java/com/product/luffy/utils/response/ResponseObject.java
+++ src/main/java/com/product/luffy/utils/response/ResponseObject.java
@@ -1,18 +1,3 @@
-/**
- * <pre>
- * Copyright (c) 2014 SamsungSDS, Inc.
- * All right reserved.
- * 
- * This software is the confidential and proprietary information of Samsung
- * SDS, Inc. You shall not disclose such Confidential Information and
- * shall use it only in accordance with the terms of the license agreement
- * you entered into with SamsungSDS.
- * 
- * Author : YongWoon Lee
- * Date : 2014. 4. 26.
- * Description : 
- * </pre>
-*/
 package com.product.luffy.utils.response;
 
 public class ResponseObject<T> {
@@ -20,7 +5,6 @@ public class ResponseObject<T> {
 	private ResultCode resultCode = HttpResultCode.PRODUCT_SUCCESS;
 	private String message;
 	private Object data;
-	private int statusMsgCd;
 
 
 	public Integer getResultCode() {
@@ -47,11 +31,4 @@ public class ResponseObject<T> {
 		this.data = object;
 	}
 
-    public int getStatusMsgCd() {
-	    return statusMsgCd;
-    }
-
-    public void setStatusMsgCd(int statusMsgCd) {
-	    this.statusMsgCd = statusMsgCd;
-    }
 }
