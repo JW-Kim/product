5875ec96bb83aa8cfc9415b694cc5f617acdee8d
JW-Kim <jw4005.kim@gmail.com>
2018-09-06 15:40:19 +0900

    Update UserController.java

--- src/main/java/com/product/luffy/controller/UserController.java
+++ src/main/java/com/product/luffy/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.product.luffy.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.product.luffy.po.User;
 import com.product.luffy.service.UserService;
+import com.product.luffy.utils.response.GridResponseObject;
+import com.product.luffy.utils.response.HttpResultCode;
 
 @Controller
 @RequestMapping("user")
@@ -16,11 +22,14 @@ public class UserController {
 	UserService userService;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public @ResponseBody Object selectUserList() {
-		Object object = new Object();
+	public @ResponseBody GridResponseObject<User> selectUserList() {
+		GridResponseObject<User> gridResponseObject = new GridResponseObject<User>();
 		System.out.println(">>>> selectUserList 시작");
-		userService.selectUserList();
+		
+		List<User> userList = userService.selectUserList();
+		gridResponseObject.setData(userList);
+		gridResponseObject.setResultCode(HttpResultCode.PRODUCT_SUCCESS);
 		System.out.println(">>>> selectUserList 끝");
-		return object;
+		return gridResponseObject;
 	}
 }
