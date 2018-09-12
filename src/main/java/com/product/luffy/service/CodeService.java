835be75f63abc190881414d6d9f1c1aedb634bca
JW-Kim <jw4005.kim@gmail.com>
2018-09-12 15:00:54 +0900

    Create CodeService.java

---
+++ src/main/java/com/product/luffy/service/CodeService.java
@@ -0,0 +1,9 @@
+package com.product.luffy.service;
+
+import java.util.Map;
+
+import com.product.luffy.po.Code;
+
+public interface CodeService {
+	public Code selectCode(Map<String, Object> paramMap);
+}
