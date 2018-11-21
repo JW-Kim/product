0d92354a04ae2ca990fc1e237ea11a4069d2779d
JW-Kim <jw4005.kim@gmail.com>
2018-11-21 10:32:15 +0900

    Update File.java

--- src/main/java/com/product/luffy/po/File.java
+++ src/main/java/com/product/luffy/po/File.java
@@ -1,38 +1,53 @@
-package com.product.luffy.controller;
+package com.product.luffy.po;
 
-import javax.annotation.Resource;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.product.luffy.po.File;
-import com.product.luffy.service.FileService;
-import com.product.luffy.utils.response.HttpResultCode;
-import com.product.luffy.utils.response.ResponseObject;
-
-@Controller
-@RequestMapping("/file")
-public class FileController {
-	private static final Logger LOGGER = LoggerFactory.getLogger(FileController.class);
-	
-	@Resource(name = "com.product.luffy.service.impl.FileService")
-	private FileService fileService;
-	
-	@RequestMapping(value="/upload", method = RequestMethod.POST)
-	public @ResponseBody ResponseObject<File> uploadFile(@RequestParam("image") MultipartFile multipartFile){
-		ResponseObject<File> responseObject = new ResponseObject<File>();
-		LOGGER.debug(">>>>>>>>>> uploadFile 시작 "+ multipartFile.getName()+", "+ multipartFile.getOriginalFilename());
-		
-		File file = fileService.insertFile(multipartFile);
-		responseObject.setData(file);
-		responseObject.setResultCode(HttpResultCode.PRODUCT_SUCCESS);
-		
-		return responseObject;
+public class File {
+	private String fileId;
+	private String fileNm;
+	private String fileExtNm;
+	private String fileSize;	
+	private String filePath;
+	private String regDtm;
+	public String getFileId() {
+		return fileId;
 	}
+	public void setFileId(String fileId) {
+		this.fileId = fileId;
+	}
+	public String getFileNm() {
+		return fileNm;
+	}
+	public void setFileNm(String fileNm) {
+		this.fileNm = fileNm;
+	}
+	public String getFileExtNm() {
+		return fileExtNm;
+	}
+	public void setFileExtNm(String fileExtNm) {
+		this.fileExtNm = fileExtNm;
+	}
+	public String getFileSize() {
+		return fileSize;
+	}
+	public void setFileSize(String fileSize) {
+		this.fileSize = fileSize;
+	}
+	public String getFilePath() {
+		return filePath;
+	}
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	public String getRegDtm() {
+		return regDtm;
+	}
+	public void setRegDtm(String regDtm) {
+		this.regDtm = regDtm;
+	}
+	@Override
+	public String toString() {
+		return "File [fileId=" + fileId + ", fileNm=" + fileNm + ", fileExtNm=" + fileExtNm + ", fileSize=" + fileSize
+				+ ", filePath=" + filePath + ", regDtm=" + regDtm + "]";
+	}
+	
+	
 }
