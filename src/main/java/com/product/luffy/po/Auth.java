a4f85fd74187da502553274ea6a3ed2e2292a1be
JW-Kim <jw4005.kim@gmail.com>
2018-11-22 17:08:05 +0900

    Create Auth.java

---
+++ src/main/java/com/product/luffy/po/Auth.java
@@ -0,0 +1,40 @@
+package com.product.luffy.po;
+
+public class Auth {
+	//User
+	private String userId;
+	private String userLoginId;
+	private String userNm;
+	private String userRole;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserLoginId() {
+		return userLoginId;
+	}
+	public void setUserLoginId(String userLoginId) {
+		this.userLoginId = userLoginId;
+	}
+	public String getUserNm() {
+		return userNm;
+	}
+	public void setUserNm(String userNm) {
+		this.userNm = userNm;
+	}
+	public String getUserRole() {
+		return userRole;
+	}
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+	
+	@Override
+	public String toString() {
+		return "Auth [userId=" + userId + ", userLoginId=" + userLoginId + ", userNm=" + userNm + ", userRole="
+				+ userRole + "]";
+	}
+	
+}
