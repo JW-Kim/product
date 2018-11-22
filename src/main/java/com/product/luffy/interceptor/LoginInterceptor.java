b25afc624cf620ea6ab58403ad8406551d94165a
JW-Kim <jw4005.kim@gmail.com>
2018-11-22 17:06:07 +0900

    Update LoginInterceptor.java

--- src/main/java/com/product/luffy/interceptor/LoginInterceptor.java
+++ src/main/java/com/product/luffy/interceptor/LoginInterceptor.java
@@ -1,18 +1,22 @@
 package com.product.luffy.interceptor;
 
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-/**
- * 登录验证拦截器
- * Author: markliu
- * Time  : 16-8-27 上午9:32
- */
-public class LoginInterceptor implements HandlerInterceptor {
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.product.luffy.po.Auth;
+import com.product.luffy.utils.UserContext;
+import com.product.luffy.utils.Exception.ProductRuntimeException;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter {
 
 	/**
 	 * Intercept the execution of a handler. Called after HandlerMapping determined an
@@ -22,31 +26,45 @@ public class LoginInterceptor implements HandlerInterceptor {
 							 HttpServletResponse response,
 							 Object handler) throws Exception {
 
-		System.out.println("LoginInterceptor preHandle...");
-
-		// 对于不需要登录验证的 url (公开url)直接放行 return true
+		System.out.println(">>>>>>>>>>>>>>>>>>>> LoginInterceptor preHandle...");
 		String requestURL = request.getRequestURI();
-		System.out.println("requestURL: " + requestURL);
 
-		// 此处对公开url硬编码，实际应该将公开url配置到配置文件中
-		if (requestURL.contains("login") || requestURL.contains("index")) {
-			return true;
+		if("/product/login".equals(requestURL)) {
+			
+		}else {
+			String authorization = request.getHeader("authorization");
+			if("Bearer".equals(authorization.split(" ")[0])) {
+				String token = authorization.split(" ")[1];
+				try {
+				    Algorithm algorithm = Algorithm.HMAC256("luffy-jw4005.kim");
+				    JWTVerifier verifier = JWT.require(algorithm)
+				        .withIssuer("auth0")
+				        .build(); 
+				    DecodedJWT jwt = verifier.verify(token);
+				    Claim userId = jwt.getClaim("userId");
+				    Claim userloginId = jwt.getClaim("userloginId");
+				    Claim userNm = jwt.getClaim("userNm");
+				    Claim userRole = jwt.getClaim("userRole");
+				    
+				    Auth auth = new Auth();
+				    auth.setUserId(userId.asString());
+				    auth.setUserLoginId(userloginId.asString());
+				    auth.setUserNm(userNm.asString());
+				    auth.setUserRole(userRole.asString());
+				    
+				    UserContext.put(auth);
+				    
+				} catch (JWTVerificationException exception){
+					throw new ProductRuntimeException("인증에 실패하였습니다.");
+				}
+			}else {
+				throw new ProductRuntimeException("인증에 실패하였습니다.");
+			}
 		}
-		HttpSession session = request.getSession();
-		String username = (String) session.getAttribute("username");
-		if (username != null) return true;
-
-		// 服务器内部请求转发打牌到登录页面
-		request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
-		return false;
+		
+		return super.preHandle(request, response, handler);
 	}
 
-	/**
-	 * Intercept the execution of a handler. Called after HandlerAdapter actually
-	 * invoked the handler, but before the DispatcherServlet renders the view.
-	 * Can expose additional model objects to the view via the given ModelAndView.
-	 * 在调用 Handler 方法之后，返回 ModelAndView 之前拦截，可用于在ModelAndView 中设置全局模型数据
-	 */
 	public void postHandle(HttpServletRequest request,
 						   HttpServletResponse response,
 						   Object handler,
@@ -55,13 +73,7 @@ public class LoginInterceptor implements HandlerInterceptor {
 		System.out.println("LoginInterceptor postHandle...");
 	}
 
-	/**
-	 * Callback after completion of request processing, that is, after rendering
-	 * the view. Will be called on any outcome of handler execution, thus allows
-	 * for proper resource cleanup or **log process**.
-	 * Handler 方法执行结束返回 modelAndView 之后执行，可进行相关 cleanup 或全局的日志记录操作。
-	 * 全局的日志记录操作时，需要将该拦截器放置到配置的全局拦截器链的第一个位置
-	 */
+
 	public void afterCompletion(HttpServletRequest request,
 								HttpServletResponse response,
 								Object handler,
