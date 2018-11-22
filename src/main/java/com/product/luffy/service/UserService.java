572c34d703f6caa9a29708b873946a0d1cde3551
JW-Kim <jw4005.kim@gmail.com>
2018-11-22 17:04:45 +0900

    Update UserService.java

--- src/main/java/com/product/luffy/service/UserService.java
+++ src/main/java/com/product/luffy/service/UserService.java
@@ -1,9 +1,12 @@
 package com.product.luffy.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.product.luffy.po.User;
 
 public interface UserService {
 	public List<User> selectUserList();
+	
+	public User selectUser(Map<String, String> paramMap);
 }
