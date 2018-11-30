fc4ae3f12f0c8ea0ed33bc039c4eef0f0bf5a8d7
JW-Kim <jw4005.kim@gmail.com>
2018-11-30 15:52:58 +0900

    Update DiaryService.java

--- src/main/java/com/product/luffy/service/DiaryService.java
+++ src/main/java/com/product/luffy/service/DiaryService.java
@@ -1,12 +1,15 @@
 package com.product.luffy.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.product.luffy.po.Diary;
 
 public interface DiaryService {
 	
 	public List<Diary> selectDiaryList(String noteId);
+	
+	public List<Diary> selectMonthDiaryList(Map<String, String> paramMap);
 	
 	public Diary selectDiary(String diaryId);
 	
