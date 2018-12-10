f288a1a4c0e0b13475ffed9272bf7399f4048733
JW-Kim <jw4005.kim@gmail.com>
2018-12-10 18:07:57 +0900

    Update DiaryMapper.java

--- src/main/java/com/product/luffy/mapper/DiaryMapper.java
+++ src/main/java/com/product/luffy/mapper/DiaryMapper.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.stereotype.Repository;
 
 import com.product.luffy.po.Diary;
+import com.product.luffy.po.Disease;
 
 @Repository("com.product.luffy.mapper.DiaryMapper")
 public interface DiaryMapper {
@@ -13,14 +14,17 @@ public interface DiaryMapper {
 	
 	public List<Diary> selectMonthDiaryList(Map<String, String> paramMap);
 	
-	
 	public Diary selectDiary(Map<String, String> paramMap );
 	
 	public int insertDiary(Diary diary);
 	
 	public int insertState(Diary diary);
 	
+	public int insertDisease(Disease disease);
+	
 	public int updateDiary(Diary diary);
 	
 	public int updateState(Diary diary);
+	
+	public int updateDisease(Disease disease);
 }
